e4eaec7fe6382eb7ddc665ce86f6e12425fac2de
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-01-19 01:42:57 +1100

    setup CORS Headers and coded initial get endpoints

--- Project_02_trivia_api/backend/flaskr/__init__.py
+++ Project_02_trivia_api/backend/flaskr/__init__.py
@@ -8,33 +8,92 @@ from backend.db.models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+#Utility function for handling pagination
+def paginate_questions(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
   
   '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+  @TODO COMPLETED: Set up CORS(Cross Origin Resource Sharing). Allow '*' for all origins. 
+    Delete the sample route after completing the TODOs
   '''
+  CORS(app, resources={"/":{"origin": "*"}})
+
+  
+  '''
+  @TODO COMPLETED: CORS Headers. Use the after_request decorator to set Access-Control-Allow
+  '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS')
+    return response
+
 
   '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
-
-  '''
-  @TODO: 
+  @TODO COMPLETED: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories')
+  def get_categories():
+    categories = Category.query.order_by(Category.id).all()
 
+    #abort if no categories found
+    if len(categories) == 0:
+      abort(404)
+
+    categories_dict = {}
+    for category in categories:
+      categories_dict[category.id] = category.type
+    
+    return jsonify({
+      "success": True,
+      "categories": categories_dict
+    })
 
   '''
-  @TODO: 
+  @TODO COMPLETED: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
+  number of total questions, current category, categories.
+  '''
+  @app.route('/questions')
+  def get_questions():
+    selection = Question.query.all()
+    total_questions = len(selection)
+    current_questions = paginate_questions(request, selection)
 
+    #abort if no questions to be shown for the current paginated page
+    if len(current_questions) == 0:
+      abort(404)
+
+    categories = Category.query.order_by(Category.id).all()
+    categories_dict = {}
+    for category in categories:
+      categories_dict[category.id] = category.type
+    
+    return jsonify({
+      "success": True,
+      "questions": current_questions,
+      "total_questions": total_questions,
+      "categories": categories_dict,
+    })
+
+
+  '''
   TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
@@ -96,8 +155,39 @@ def create_app(test_config=None):
   '''
   @TODO: 
   Create error handlers for all expected errors 
-  including 404 and 422. 
+  including 400, 404, 422 and 500. 
   '''
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      "success": False,
+      "error": 400,
+      "message": "bad request"
+    }),400
+  
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      "success": False,
+      "error": 404,
+      "message": "resource not found"
+    }),404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False,
+      "error": 422,
+      "message": "unprocessable"
+    }),422
+
+  @app.errorhandler(500)
+  def internal_server_error(error):
+    return jsonify({
+      "success": False,
+      "error": 500,
+      "message": "internal server error"
+    }),500
   
   return app
 
