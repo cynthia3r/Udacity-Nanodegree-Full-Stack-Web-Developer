9e7297ff48afe47aff8d2f7ea29004ec372ed6f3
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-01-26 11:34:08 +1100

    style: adhered to PEP8 code style guide

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from flask import (
+        Flask,
+        render_template,
+        request,
+        redirect,
+        url_for, jsonify,
+        abort
+)
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
@@ -10,27 +17,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class TodoList(db.Model):
     __tablename__ = 'todolists'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    todos = db.relationship('Todo', backref='list', lazy=True, cascade="all, delete-orphan")
+    todos = db.relationship('Todo',
+                            backref='list',
+                            lazy=True,
+                            cascade="all, delete-orphan")
 
     def __repr__(self):
         return f'<TodoList ID: {self.id}, name: {self.name}, todos: {self.todos}>'
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    todolist_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
+    todolist_id = db.Column(db.Integer,
+                            db.ForeignKey('todolists.id'),
+                            nullable=False)
 
     def __repr__(self):
         return f'<Todo ID: {self.id}, description: {self.description}, complete: {self.completed}>'
 
-#db.create_all()
+# db.create_all()
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -41,7 +56,9 @@ def create_todo():
         description = request.get_json()['description']
         list_id = request.get_json()['todolist_id']
         print("list_id: ", list_id)
-        todo = Todo(description=description, completed=False, todolist_id=list_id)
+        todo = Todo(description=description,
+                    completed=False,
+                    todolist_id=list_id)
         db.session.add(todo)
         db.session.commit()
         body['id'] = todo.id
@@ -58,6 +75,7 @@ def create_todo():
         abort(500)
     else:
         return jsonify(body)
+
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed(todo_id):
@@ -80,15 +98,16 @@ def set_completed(todo_id):
     else:
         return '', 200
 
+
 @app.route('/todos/<todo_id>/delete', methods=['DELETE'])
 def delete_todo(todo_id):
-    error=False
+    error = False
     try:
         Todo.query.filter_by(id=todo_id).delete()
         db.session.commit()
     except:
         db.session.rollback()
-        error=True
+        error = True
     finally:
         db.session.close()
 
@@ -121,6 +140,7 @@ def create_lists():
     else:
         return jsonify(body)
 
+
 @app.route('/lists/<list_id>/delete', methods=['DELETE'])
 def delete_list(list_id):
     error = False
@@ -133,7 +153,7 @@ def delete_list(list_id):
         db.session.commit()
     except:
         db.session.rollback()
-        error=True
+        error = True
     finally:
         db.session.close()
 
@@ -141,6 +161,7 @@ def delete_list(list_id):
         abort(500)
     else:
         return jsonify({'success': True})
+
 
 @app.route('/lists/<list_id>/set-completed', methods=['POST'])
 def set_list_completed(list_id):
@@ -169,14 +190,15 @@ def set_list_completed(list_id):
 
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
-    return render_template('index.html', 
-    lists = TodoList.query.all(),
-    active_list = TodoList.query.get(list_id),
-    todos = Todo.query.filter_by(todolist_id=list_id).order_by('id').all())
+    return render_template('index.html',
+        lists=TodoList.query.all(),
+        active_list=TodoList.query.get(list_id),
+        todos=Todo.query.filter_by(todolist_id=list_id).order_by('id').all())
+
 
 @app.route('/')
 def index():
     return redirect(url_for('get_list_todos', list_id=1))
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
